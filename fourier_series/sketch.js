c8134564dad43574a0adfb5d13e9a48a9005615a
Wills Liou <willsliou@protonmail.com>
2020-10-12 17:50:49 -0700

    sketch

--- fourier_series/sketch.js
+++ fourier_series/sketch.js
@@ -1,3 +1,4 @@
+// Initialize global variables
 let time = 0;
 let wave = [];
 
@@ -7,34 +8,45 @@ function setup() {
 }
 
 function draw() {
+    // Draw black background
     background(0);
-    translate(200, 200);
+    // Init variables
     let radius = 100;
-
-    stroke(255);
-    noFill();
-    ellipse(0, 0,radius * 2);
-
     let x = radius * cos(time);
     let y = radius * sin(time);
-    wave.unshift(y);
-    fill(255);
-    // Draw line
-    line(0,0,x,y);
-    // Draw ball at end of line
-    ellipse(x, y,10);
 
-    // Draw link from ball to graph and sync.
+    // Draw border of circle white
+    stroke(255);
+    // Make circle empty
+    noFill();
+    // Move our circle down a bit
+    translate(200, 200);
+    // Draw main circle
+    ellipse(0, 0, radius * 2);
+
+
+    // fill(255);
+    // Draw line in main circle
+    line(0, 0, x, y);
+    // // Draw ball at end of line
+    ellipse(x, y, 10);
+    // // Draw link from ball to graph and sync.
     translate(200,0);
+    // Draw flat, connector line
     line(x - 200, y, 0, wave[0]);
+    // Add new y value at end of array wave
+    wave.unshift(y);
 
+
+    // Record shape on screen
     beginShape();
     noFill();
+    // for each item in wave, plot point
     for (let i = 0; i < wave.length; i++) {
         vertex(i, wave[i]);
     }
     endShape();
 
 
-    time += 0.03;
+    time += 0.01;
 }
